d1453062cd62a5ccaea1dec902783d292d8a4520
Harshula <harshula.jayasuriya@anu.edu.au>
2022-10-24 17:11:24 +1100

    Synchronise with the latest changes to oasis3-mct
    * Point oasis3-mct submodule to track branch improve-build-system.
    * Stop using the PLATFORM env variable since oasis3-mct now has a
      build.sh script.

--- build.sh
+++ build.sh
@@ -3,12 +3,10 @@
 PROGNAME="$( basename ${BASH_SOURCE[0]} )"
 PROJDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDDIR="${PROJDIR}/build"
-PLATFORM=""
 
 if hostname --fqdn | grep gadi.nci.org.au$ > /dev/null
 then
 	echo "${PROGNAME}: Set up environment on gadi.nci.org.au"
-	PLATFORM="nci"
 	module purge
 	module load intel-compiler/2019.5.281
 	module load netcdf/4.7.4
@@ -20,5 +18,5 @@ echo -e "${PROGNAME}: executing cmake with \$PATH set to: $PATH\n"
 
 rm -rf ${BUILDDIR} && \
 mkdir -p ${BUILDDIR} && \
-cmake -S ${PROJDIR} -B ${BUILDDIR} -DPLATFORM="${PLATFORM}" && \
+cmake -S ${PROJDIR} -B ${BUILDDIR} && \
 cmake --build ${BUILDDIR} --verbose
